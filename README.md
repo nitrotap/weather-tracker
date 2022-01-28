f583caaf17838f116a6943d50b2b08446ef728bd
nitrotap <kartikinpublic@gmail.com>
2022-01-28 01:48:14 -0700

    setting up bootstrap, fontawesome, and fetch logic

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Weather Tracker
 
 Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
 
 Use the [OpenWeather One Call API](https://openweathermap.org/api/one-call-api) to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use `localStorage` to store any persistent data.
+
+
+## TODO 
+https://openweathermap.org/api/one-call-api
+https://openweathermap.org/api/geocoding-api
+
+
+
+
 
 ## User Story
 
---
+++ assets/js/search.js
@@ -0,0 +1,88 @@
+/*
+ * js file for location search bar
+ *
+ */
+
+let searchButtonEl = document.querySelector("#search-button");
+
+searchButtonEl.addEventListener("click", function() {
+    let cityInputEl = document.querySelector("#cityInput");
+    let cityName = cityInputEl.value;
+    searchCityRequest(cityName);
+
+
+})
+
+
+let city;
+
+
+let searchCityRequest = function(cityName) {
+    let requestUrl =
+        "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=63fc07461258d8d00c91ca3f94112536";
+    fetch(requestUrl)
+        .then(function (response) { // response is an HTTP response that needs to be converted into JSON
+            if (response.status !== 200) {  // if you get a status code of not 200 (status OK)
+                document.location.replace('./404.html'); // then replace with 404 page
+            } else {
+                return response.json(); // returns JSON
+            }
+        })
+        .then(function (data) {
+            // pass data into function parsing the data
+            /*
+            console.log(data)
+            console.log(data[0].name)
+            console.log(data[0].lat)
+            console.log(data[0].lon)*/
+
+            // make another function - pass json data into it, parse the object
+            // 2api requetss, but need to wait until first finishes, then
+
+            city = {
+                name: data[0].name,
+                lat: data[0].lat,
+                lon: data[0].lon
+            };
+            console.log(city); // this object is only loading AFTER the fetch
+
+            fetchCityWeather(city.lat, city.lon);
+            // once all data
+            fetchCityWeather(data);
+        }).catch(function (error) {
+            console.log(error);
+    });
+}
+
+const fetchCityWeather = function(lat, lon) {
+    let requestUrl =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +
+        "&lon="+ lon + "&appid=63fc07461258d8d00c91ca3f94112536";
+    fetch(requestUrl)
+        .then(function(response) { // response is an HTTP response that needs to be converted into JSON
+            if (response.status !== 200) {  // if you get a status code of not 200 (status OK)
+                document.location.replace('./404.html'); // then replace with 404 page
+            } else {
+                return response.json; // returns JSON
+            }})
+        .then(function(data) {
+            console.log(data);
+            // displaying the data - a new function that shows forecast
+            // invoke the show forecast function
+        }).catch(function (error) {
+        console.log(error);
+    });
+
+}
+
+
+
+let testFunction = function() {
+    console.log("hi");
+}
+
+// ready function - need to wrap things with "onload, wait"
+    // modules export whats in one file, then its available in another file
+    // clump by what its doing , and try avoid sharing between files. if not possible, then can use above
+    // set it up for each page to have set of javascript
+
